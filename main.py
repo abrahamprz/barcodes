caae6f0b97d93aa0aa82a1bec9c7fc5783cc339b
abrahamprz <fcoabrahamprz@gmail.com>
2023-12-07 15:50:12 -0700

    Update columns_to_replace in main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from barcode_pdf import BarcodePDF
 
 
 def main():
-    columns_to_replace = ["DepartmentTag", "PersonId"]
+    columns_to_replace = ["DepartmentTag", "PersonId"] # ["DepartmentTag", "PersonID"]
     barcode_pdf = BarcodePDF("example.csv")
     missing_values = barcode_pdf.generate_pdf(barcode_columns=columns_to_replace)
     
